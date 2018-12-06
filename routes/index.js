78ad655cd6f1f9772a9ba6a61fffa13d9571e869
unknown <user05@IUTPARIS.LOCAL>
2018-12-06 23:13:56 +0100

    Test routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path');
+//var path = require('path');
 
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../views', 'index.html'));
+  //res.sendFile(path.join(__dirname, '../views', 'index.html'));
+  res.render('index');
 });
 
 module.exports = router;
